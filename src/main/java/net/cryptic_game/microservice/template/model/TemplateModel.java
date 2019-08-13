968207df839892d67e23b9d5484a0bf333bd63df
Jannik Emmerich <mail@jannik-emmerich.de>
2019-08-13 17:11:47 +0200

    Enhanced from raw sql to orm

--- src/main/java/net/cryptic_game/microservice/template/model/TemplateModel.java
+++ src/main/java/net/cryptic_game/microservice/template/model/TemplateModel.java
@@ -2,25 +2,57 @@ package net.cryptic_game.microservice.template.model;
 
 import java.util.UUID;
 
+import net.cryptic_game.microservice.db.Database;
 import net.cryptic_game.microservice.model.Model;
+import org.hibernate.Session;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "template")
 public class TemplateModel extends Model {
 	
 	public TemplateModel() {
-		super("template");
+		//TODO
 	}
 	
 	public void update() {
-		// TODO
+		Session session = Database.getInstance().openSession();
+		session.beginTransaction();
+
+		//TODO
+
+		session.update(this);
+
+		session.getTransaction().commit();
+		session.close();
 	}
 	
 	public static TemplateModel get(UUID uuid) {
-		// TODO
-		return null;
+		Session session = Database.getInstance().openSession();
+		session.beginTransaction();
+
+		TemplateModel model = session.get(TemplateModel.class, uuid);
+
+		session.getTransaction().commit();
+		session.close();
+
+		return model;
 	}
 	
 	public static TemplateModel create() {
-		// TODO
+		Session session = Database.getInstance().openSession();
+		session.beginTransaction();
+
+		TemplateModel model = new TemplateModel();
+
+		//TODO
+
+		session.save(model);
+
+		session.getTransaction().commit();
+		session.close();
 		return null;
 	}
 
