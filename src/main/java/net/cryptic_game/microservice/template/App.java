b92e9eaffb4c595aa5dc3e7ba232d76fc7032349
Jannik Emmerich <mail@jannik-emmerich.de>
2019-08-22 20:07:05 +0200

    Remove initialize database

--- src/main/java/net/cryptic_game/microservice/template/App.java
+++ src/main/java/net/cryptic_game/microservice/template/App.java
@@ -1,16 +1,12 @@
 package net.cryptic_game.microservice.template;
 
 import net.cryptic_game.microservice.MicroService;
-import net.cryptic_game.microservice.db.Database;
 import org.apache.log4j.BasicConfigurator;
 
 public class App extends MicroService {
 
 	private App() {
 		super("template");
-
-		// if needed
-		new Database();
 	}
 
 	public static void main(String[] args) {
